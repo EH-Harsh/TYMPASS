54178d16820b2decfdac78e98676570ab4e8b1b9
EH-Harsh <harshkumarhk1930@gmail.com>
2025-07-01 16:11:10 +0530

    Code Added

---
+++ C/Array/basicarray.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main (){
+    int arr[5]={1,2,3,4,5};
+    //printf("%d\n",arr[0]);
+    //printf("%d\n",arr[1]);
+    for(int i=0;i<5;i++){
+        printf("%d\n",arr[i]);
+    }
+    return 0;
+}
