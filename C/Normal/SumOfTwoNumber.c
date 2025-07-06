d0645cd0a7d694d6a64a9133695c077da0778206
EH-Harsh <harshkumarhk1930@gmail.com>
2025-07-06 22:47:44 +0530

    Added SumOfTwoNumber.c

---
+++ C/Normal/SumOfTwoNumber.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main()
+{
+    int x,y,sum;
+    printf("enter first second number");
+    scanf("%d %d",&x,&y);
+    printf("Sum of two number:%d",sum=x+y);
+    
+    return 0;
+}
